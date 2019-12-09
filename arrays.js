ef5abe0044610195ad953a861a419bf4bb0f4076
Antonio Dinkins <afdinki@gmail.com>
2019-12-09 22:17:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,15 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-    //var newArr = array
-    //newArr.unshift(element)
-    return array.unshift(element)
+  var newArr = []
+  newArr = array.map(item => newArr.push(item))
+  newArr.unshift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element){
-  var newArr = array
+  var newArr = []
+  newArr = array.map(item => newArr.push(item))
   newArr.push(element)
   return newArr
 }
