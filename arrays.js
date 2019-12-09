a71ef6ef32ffce918e9be10a5a86cd4a8573bcd3
Antonio Dinkins <afdinki@gmail.com>
2019-12-09 21:56:28 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+    let newArr = arr.unshift(element)
+    return newArr
+}
+
+function addElementToEndOfArray(array, element){
+  let newArr = arr.push(element)
+  return newArr
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return arr.unshift(element)
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return arr.pop(element)
+}
+
+const accessElementInArray = (arr, index) => {
+  return arr[index]
+}
